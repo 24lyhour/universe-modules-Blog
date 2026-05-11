7dad0c7e42acc20bf34b514d420b16f2ea978922
Lyhour <kouchlyhour@gmail.com>
2026-05-11 09:45:06 +0700

    chore: comment out post management menu items in DashboardMiddlewareHandle

--- app/Http/Middleware/DashboardMiddlewareHandle.php
+++ app/Http/Middleware/DashboardMiddlewareHandle.php
@@ -36,8 +36,8 @@ class DashboardMiddlewareHandle
             'blog.*'
         );
 
-        MenuService::addSubmenuItem('primary', 'blog', __('All Posts'), '/dashboard/blog', 10, 'posts.view_any', 'blog.index', 'List');
-        MenuService::addSubmenuItem('primary', 'blog', __('Create Post'), '/dashboard/blog/create', 20, 'posts.create', 'blog.create', 'Plus');
+        // MenuService::addSubmenuItem('primary', 'blog', __('All Posts'), '/dashboard/blog', 10, 'posts.view_any', 'blog.index', 'List');
+        // MenuService::addSubmenuItem('primary', 'blog', __('Create Post'), '/dashboard/blog/create', 20, 'posts.create', 'blog.create', 'Plus');
         MenuService::addSubmenuItem('primary', 'blog', __('Banners'), route('blog.banners.index'), 30, 'banners.view_any', 'blog.banners.*', 'Image');
         MenuService::addSubmenuItem('primary', 'blog', __('Special Offers'), route('blog.special-offers.index'), 40, 'special_offers.view_any', 'blog.special-offers.*', 'Tag');
         MenuService::addSubmenuItem('primary', 'blog', __('Slider Shows'), route('blog.slider-shows.index'), 50, 'slider_shows.view_any', 'blog.slider-shows.*', 'PanelTop');
